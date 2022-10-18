525b7464d3dea6f52554e8097de787b6a8ce8a8d
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-10-18 14:56:49 -0300

    navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello DSCatalog</h1>
     </div>
   );
