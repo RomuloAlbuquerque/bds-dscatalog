c913e4180b783573b8d8e1936389f6d96dd29a6c
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-10-28 19:52:18 -0300

    Routes

---
+++ frontweb/src/pages/Admin/index.tsx
@@ -0,0 +1,114 @@
+import './styles.css';
+
+const Admin = () => {
+  return (
+    <div className="container container-main">
+      <div className="row display-admin">
+        <div className="col-lg-12">
+          <div className="adm">
+            <h1>Teste Grid System</h1>
+          </div>
+        </div>
+
+        <div className="row display-admin">
+          <div className="col-lg-6 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-6 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+        </div>
+
+        <div className="row display-admin">
+          <div className="col-lg-4 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-4 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-4 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+        </div>
+
+        <div className="row display-admin">
+          <div className="col-lg-3 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-3 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-3 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-3 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+        </div>
+
+        <div className="row display-admin">
+          <div className="col-lg-2 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-2 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-2 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-2 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-2 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-2 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+        </div>
+
+        <div className="row display-admin">
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+          <div className="col-lg-1 adm">
+            <h1>Teste Grid System</h1>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+export default Admin;
---
+++ frontweb/src/pages/Catalog/index.tsx
@@ -0,0 +1,49 @@
+import ProductCard from 'components/ProductCard';
+import './styles.css';
+
+const Catalog = () => {
+  return (
+    <div className="container my-4">
+      <div className="row">
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+          <ProductCard />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Catalog;
