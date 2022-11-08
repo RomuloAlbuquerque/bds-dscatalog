e447c7669367b50cb3f839b4e110dfb79ead3776
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-11-08 13:25:40 -0300

    ProductDetails route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from "pages/Home"
 import Catalog from "pages/Catalog"
 import Admin from "pages/Admin"
 import Navbar from "components/Navbar"
+import ProductDetails from "components/ProductDetails"
 
 const Routes = () =>(
         <BrowserRouter>
@@ -11,8 +12,11 @@ const Routes = () =>(
                 <Route path="/" exact>
                     <Home />
                 </Route>
-                <Route path="/products">
+                <Route path="/products" exact>
                     <Catalog />
+                </Route>
+                <Route path="/products/:productid">
+                    <ProductDetails />
                 </Route>
                 <Route path="/admin">
                     <Admin />
