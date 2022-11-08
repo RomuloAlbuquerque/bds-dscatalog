2befae8d146957a79899b5a962305994d5a4f01f
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-11-07 23:52:09 -0300

    format price

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -28,40 +28,40 @@ const Catalog = () => {
   return (
     <div className="container my-4">
       <div className="row">
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
-        <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <ProductCard product={product} />
         </div>
       </div>
