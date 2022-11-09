eb766575e633e90546083aa4a587cc4c90fd767a
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-11-09 18:53:56 -0300

    Routes, redirectings, nesting

--- frontweb/src/pages/Admin/Navbar/index.tsx
+++ frontweb/src/pages/Admin/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './styles.css'
 
 const Navbar = () => {
@@ -5,19 +6,19 @@ const Navbar = () => {
     <nav className="admin-navbar-content">
       <ul>
         <li>
-          <a href="link" className='admin-link-item active'>
+          <NavLink to="/admin/products" className='admin-link-item'>
             <p>Produtos</p>
-          </a>
+          </NavLink>
         </li>
         <li>
-          <a href="link" className='admin-link-item'>
+          <NavLink to="/admin/categories" className='admin-link-item'>
             <p>Categorias</p>
-          </a>
+          </NavLink>
         </li>
         <li>
-          <a href="link" className='admin-link-item'>
+          <NavLink to="/admin/users" className='admin-link-item'>
             <p>Usuários</p>
-          </a>
+          </NavLink>
         </li>
       </ul>
     </nav>
--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,12 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import './styles.css';
 
 const Admin = () => {
   return (
-    <div className='admin-container'>
+    <div className="admin-container">
       <Navbar />
-      <div className='admin-content'>
-        <h1>Conteúdo</h1>
+      <div className="admin-content">
+        <Switch>
+          <Route path="/admin/products" exact>
+            <h1>Produtos</h1>
+          </Route>
+          <Route path="/admin/categories" exact>
+            <h1>Categorias</h1>
+          </Route>
+          <Route path="/admin/users" exact>
+            <h1>Usuários</h1>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
