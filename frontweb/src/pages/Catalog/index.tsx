776e39b1117a537d8c4831fc57f1bbcedc5c9cd6
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-11-08 21:00:32 -0300

    Catalog title

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -5,7 +5,11 @@ import './styles.css';
 
 const Catalog = () => {
   return (
-    <div className="container my-4">
+    <div className="container my-4 catalog-container">
+      <div className="row catalog-title-container">
+        <h1>Catálogo de Produtos</h1>
+      </div>
+
       <div className="row">
         <div className="col-sm-6 col-md-5 col-lg-4 col-xl-3">
           <Link to="/products/1">
